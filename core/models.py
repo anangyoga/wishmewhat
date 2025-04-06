a2234df09048e6548bc7b8b270984606d17ccdf8
anangyoga <aharwinto@gmail.com>
2025-04-06 23:43:32 +0700

    models & migrations

---
+++ core/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from .utils import generate_id
+
+class BaseModel(models.Model):
+    id = models.CharField(
+        primary_key=True, max_length=100, default=generate_id, editable=False
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
--- wishmewhat/models.py
+++ wishmewhat/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+from core.models import BaseModel
+
 # Create your models here.
+class Wishes(BaseModel):
+    text = models.TextField()    
