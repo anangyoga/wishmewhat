3282db66067d31763ab183062e083f8cadf42baf
anangyoga <aharwinto@gmail.com>
2025-04-06 23:50:19 +0700

    views

--- wishmewhat/admin.py
+++ wishmewhat/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Wishes
 
 # Register your models here.
+class WishesAdmin(admin.ModelAdmin):
+    list_display = ("text", "created_at")
+
+admin.site.register(Wishes, WishesAdmin)
--- wishmewhat/views.py
+++ wishmewhat/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import Wishes
 
 # Create your views here.
+class IndexView(View):
+    def get(self, request):
+        wishes = Wishes.objects.all()
+
+        context = {
+            'wishes': wishes
+        }
+
+        return render(request, 'index.html', context)
+
+    def post(self, request):
+        text = request.POST.get('wish')
+
+        Wishes.objects.create(text=text)
+        return redirect('index')
